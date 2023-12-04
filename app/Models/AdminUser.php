77ffea0cdcb97472053990c12b2205d0c1ddb1cf
Chamud-Sachintha <sheraanmario777@gmail.com>
2023-12-04 21:59:49 +0530

    add admin user model and middlewares

---
+++ app/Models/AdminUser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AdminUser extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'token',
+        'login_time',
+        'create_time'
+    ];
+
+    public function find_by_token($token) {
+        $map['token'] = $token;
+
+        return $this->where($map)->first();
+    }
+}
