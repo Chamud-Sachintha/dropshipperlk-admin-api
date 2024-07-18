e79fa9b884ec4d1a99cb843bb31ad86d76c9289d
HashanDMS <hashanmirinda10@gmail.com>
2024-07-19 03:06:01 +0530

    admin updates

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,8 @@ use App\Models\Order;
 use App\Models\ProfitShare;
 use Illuminate\Http\Request;
 use App\Models\OrderEn;
+use App\Models\Reseller;
+use App\Models\KYCInformation;
 
 class DashboardController extends Controller
 {
@@ -15,6 +17,7 @@ class DashboardController extends Controller
     private $Reseller;
     private $ProfitShareLog;
     private $OrderEn;
+    private $KYCModel;
     
     public function __construct()
     {   
@@ -22,6 +25,8 @@ class DashboardController extends Controller
         $this->Orders = new Order();
         $this->ProfitShareLog = new ProfitShare();
         $this->OrderEn = new OrderEn();
+        $this->Reseller = new Reseller();
+        $this->KYCModel = new KYCInformation();
     }
 
     public function getDashboardData(Request $request) {
@@ -38,6 +43,13 @@ class DashboardController extends Controller
         $received_earnings = $this->ProfitShareLog->get_total_earnings();
         $pending_payment = $this->OrderEn->get_pending_payment();
 
+        $TotalResellerCount = $this->Reseller->get_total_user_count();
+        $PendingResellerCount = $this->KYCModel->get_pending_user_count();
+        $ActiveResellerCount = $this->KYCModel->get_aproved_user_count();
+
+        $returnOrders = $this->OrderEn->get_returned_order_count();
+        $holdorder = $this->OrderEn->get_hold_order_count();
+
         $dataList['pendingOrderCount'] = $pending_orders;
         $dataList['inCourierOrderCount'] = $in_courier_orders;
         $dataList['completeOrderCount'] = $Complte_corders;
@@ -47,6 +59,11 @@ class DashboardController extends Controller
         $dataList['totalEarnigs'] = $received_earnings;
         $dataList['pendingPayment'] = $pending_payment;
         $dataList['DeliverdPayment'] = $delivered_corders;
+        $dataList['TotalResellerCount'] = $TotalResellerCount;
+        $dataList['PendingResellerCount'] = $PendingResellerCount;
+        $dataList['ActiveResellerCount'] = $ActiveResellerCount;
+        $dataList['orderreturnedcount'] = $returnOrders;
+        $dataList['orderholdcount'] = $holdorder;
 
         return $this->AppHelper->responseEntityHandle(1, "Operation Complete", $dataList);
 
