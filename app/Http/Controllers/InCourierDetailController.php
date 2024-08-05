209cb796b15b29164efb3706c0a14381e00fc8d3
Chamud-Sachintha <sheraanmario777@gmail.com>
2024-08-05 10:58:29 +0530

    bug fix

--- app/Http/Controllers/InCourierDetailController.php
+++ app/Http/Controllers/InCourierDetailController.php
@@ -12,7 +12,9 @@ use App\Models\Product;
 use App\Models\ProfitShare;
 use App\Models\Reseller;
 use App\Models\ResellProduct;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 use function PHPUnit\Framework\isNull;
 
@@ -45,7 +47,8 @@ class InCourierDetailController extends Controller
         $this->City = new city_list();
     }
 
-    public function createCourierPackage(Request $request) {
+    public function createCourierPackage(Request $request)
+    {
         $orderNumber = (is_null($request->orderNumber) || empty($request->orderNumber)) ? "" : $request->orderNumber;
         $wayBillNo = (is_null($request->wayBillNumber) || empty($request->wayBillNumber)) ? "" : $request->wayBillNumber;
 
@@ -87,7 +90,8 @@ class InCourierDetailController extends Controller
         }
     }
 
-    public function updatePackageStatus(Request $request) {
+    public function updatePackageStatus(Request $request)
+    {
         $orderNumber = (is_null($request->orderNumber) || empty($request->orderNumber)) ? "" : $request->orderNumber;
         $orderStatus = (is_null($request->orderStatus) || empty($request->orderStatus)) ? "" : $request->orderStatus;
 
@@ -127,7 +131,7 @@ class InCourierDetailController extends Controller
                         $courir_charge = $this->getCourierCharge($is_city_colombo, $product_info['weight']);
 
                         // $profit = (($resell_info['price'] * $order_info['quantity']) - $product_info['price']) - $courir_charge;
-                        $profit = ($order_info['total_amount'] - ($product_info['price'] * $order_info['quantity'])) ;
+                        $profit = ($order_info['total_amount'] - ($product_info['price'] * $order_info['quantity']));
 
                         $direct_commision = ($product_info['price'] * ($product_info['direct_commision'] / 100) * $order_info['quantity']);
 
@@ -144,7 +148,7 @@ class InCourierDetailController extends Controller
                         if ($set_profit_total != null) {
                             $profitShareInfo['profitTotal'] = $set_profit_total['profit_total'];
                             $profitShareInfo['createTime'] = $this->AppHelper->get_date_and_time();
-                            
+
                             $ref_list = $this->Reseller->get_ref_list_by_seller($reseller_info['ref_code']);
                             $profit_log = $this->ProfitShare->add_log($profitShareInfo);
 
@@ -165,7 +169,7 @@ class InCourierDetailController extends Controller
 
                                 $ref_profit_info['teamCommision'] = $team_commision;
                                 $ref_profit_info['profit'] = 0;
-        
+
                                 $ref_profit_info['profitTotal'] = ($value['profit_total'] + $team_commision);
                                 $ref_profit_info['createTime'] = $this->AppHelper->get_date_and_time();
 
@@ -176,7 +180,6 @@ class InCourierDetailController extends Controller
                             }
                         }
                     }
-                    
                 }
 
                 return $this->AppHelper->responseMessageHandle(1, "Operation Complete");
@@ -186,7 +189,8 @@ class InCourierDetailController extends Controller
         }
     }
 
-    public function removeCourierPackageLog(Request $request) {
+    public function removeCourierPackageLog(Request $request)
+    {
         $id = (is_null($request->id) || empty($request->id)) ? "" : $request->id;
 
         if ($id == "") {
@@ -203,72 +207,53 @@ class InCourierDetailController extends Controller
     }
 
     public function getPackageReadyOrderList(Request $request) {
-        $pendingPackageList = $this->InCourierDetail->get_pending_list();
-
-        $pendingListArray = [];
-        foreach ($pendingPackageList as $key => $eachPackage) {
-
-            $orderInfo = $this->OrderEn->get_by_id_bulk($eachPackage['order']);
-
-            if ($orderInfo['order_status'] > 4) {
-                continue;
-            }
-
-            $pendingListArray[$key]['id'] = $eachPackage['id'];
-            $pendingListArray[$key]['orderNumber'] = $eachPackage['order'];
-            $pendingListArray[$key]['wayBillNo'] = $eachPackage['way_bill'];
-
-            $response = json_decode($this->trackPackageQuery($eachPackage['way_bill']));
-
-            if ($response->success == true) {
-                $pendingListArray[$key]['packageStatus'] = $response->data->status;
-            } else {
-                $pendingListArray[$key]['packageStatus'] = $response->message;
-            }
-
-            if ($eachPackage->package_create_status == 0) {
-                $pendingListArray[$key]['packageCreateStatus'] = "Pending";
-            } else {
-                $pendingListArray[$key]['packageCreateStatus'] = "Created";
-            }
-
-            $resellerInfo = $this->Reseller->find_by_id($orderInfo['reseller_id']);
-
-            if ($orderInfo['order_status'] == 0) {
-                $pendingListArray[$key]['orderStatus'] = "Pending";
-            } else if ($orderInfo['order_status'] == 1) {
-                $pendingListArray[$key]['orderStatus'] = "Hold";
-            } else if ($orderInfo['order_status'] == 2) {
-                $pendingListArray[$key]['orderStatus'] = "Packaging";
-            } else if ($orderInfo['order_status'] == 3) {
-                $pendingListArray[$key]['orderStatus'] = "Cancel";
-            } else if ($orderInfo['order_status'] == 4) {
-                $pendingListArray[$key]['orderStatus'] = "In Courier";
-            } else if ($orderInfo['order_status'] == 5) {
-                $pendingListArray[$key]['orderStatus'] = "Delivered";
-            } else if ($orderInfo['order_status'] == 7) {
-                $pendingListArray[$key]['orderStatus'] = "Complete ";
-            } else if ($orderInfo['order_status'] == 8) {
-                $pendingListArray[$key]['orderStatus'] = "Settled ";
-            } else if ($orderInfo['order_status'] == 9) {
-                $pendingListArray[$key]['orderStatus'] = "Return Recieved ";
-            } else if ($orderInfo['order_status'] == 10) {
-                $pendingListArray[$key]['orderStatus'] = "Ready to Change ";
-            } else if ($orderInfo['order_status'] == 11) {
-                $pendingListArray[$key]['orderStatus'] = "Rescheduled ";
-            } else {
-                $pendingListArray[$key]['orderStatus'] = "Return Order";
-            }
-
-            $pendingListArray[$key]['resellerName'] = $resellerInfo['b_name'];
-            $pendingListArray[$key]['refCode'] = $resellerInfo['ref_code'];
-            $pendingListArray[$key]['createTime'] = $eachPackage['create_time'];
-        }
-
+        $pendingListArray = DB::table('in_courier_details')
+            ->join('order_ens', 'in_courier_details.order', '=', 'order_ens.order')
+            ->join('resellers', 'order_ens.reseller_id', '=', 'resellers.id')
+            ->select('in_courier_details.*', 'order_ens.order_status', 'resellers.b_name', 'resellers.ref_code')
+            ->where('order_ens.order_status', '<=', 4)
+            ->get()
+            ->map(function ($eachPackage) {
+                $response = json_decode($this->trackPackageQuery($eachPackage->way_bill));
+    
+                return [
+                    'id' => $eachPackage->id,
+                    'orderNumber' => $eachPackage->order,
+                    'wayBillNo' => $eachPackage->way_bill,
+                    'packageStatus' => $response->success ? $response->data->status : $response->message,
+                    'packageCreateStatus' => $eachPackage->package_create_status == 0 ? "Pending" : "Created",
+                    'orderStatus' => $this->mapOrderStatus($eachPackage->order_status),
+                    'resellerName' => $eachPackage->b_name,
+                    'refCode' => $eachPackage->ref_code,
+                    'createTime' => $eachPackage->create_time,
+                ];
+            });
+    
         return $this->AppHelper->responseEntityHandle(1, "Operation Complete", $pendingListArray);
     }
+    
+    private function mapOrderStatus($status) {
+        $orderStatusMap = [
+            0 => 'Pending',
+            1 => 'Hold',
+            2 => 'Packaging',
+            3 => 'Cancel',
+            4 => 'In Courier',
+            5 => 'Delivered',
+            7 => 'Complete',
+            8 => 'Settled',
+            9 => 'Return Received',
+            10 => 'Ready to Change',
+            11 => 'Rescheduled',
+            6 => 'Return Order'
+        ];
+        return $orderStatusMap[$status] ?? 'Unknown Status';
+    }
+    
 
-    private function ceylonxDeliveryOrderPlacement($orderInfo) {
+
+    private function ceylonxDeliveryOrderPlacement($orderInfo)
+    {
         $ch = curl_init();
 
         $data = http_build_query(array(
@@ -299,9 +284,10 @@ class InCourierDetailController extends Controller
         return $server_output;
     }
 
-    private function trackPackageQuery($wayBillNo) {
+    private function trackPackageQuery($wayBillNo)
+    {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,"https://api.ceylonex.lk/api/v1/get-package?api_key=" . $this->APP_KEY . "&waybill=" . $wayBillNo);
+        curl_setopt($ch, CURLOPT_URL, "https://api.ceylonex.lk/api/v1/get-package?api_key=" . $this->APP_KEY . "&waybill=" . $wayBillNo);
         curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-formurlencoded'));
         // receive server response ...
@@ -309,15 +295,16 @@ class InCourierDetailController extends Controller
 
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        
-        $server_output = curl_exec ($ch);
 
-        curl_close($ch); 
+        $server_output = curl_exec($ch);
+
+        curl_close($ch);
 
         return $server_output;
     }
 
-    private function isCityinsideColombo($city) {
+    private function isCityinsideColombo($city)
+    {
         $is_colombo_city = $this->City->validate_city($city, "Colombo");
 
         if ($is_colombo_city) {
@@ -327,7 +314,8 @@ class InCourierDetailController extends Controller
         }
     }
 
-    private function getCourierCharge($is_colombo, $product_weight) {
+    private function getCourierCharge($is_colombo, $product_weight)
+    {
 
         $default_charge = 350;
         $weight_in_kg = ($product_weight) / 1000;
@@ -335,7 +323,7 @@ class InCourierDetailController extends Controller
         if ($weight_in_kg > 1) {
             $remaining = $weight_in_kg - 1;
             $round_remaining = ceil($remaining);
-            
+
             if ($round_remaining > 0) {
                 $default_charge += ($round_remaining * 50);
             }
